1619e70821d963f45eba6d10c1fe807fd268b228
phanirithvij <phanirithvij2000@gmail.com>
2026-08-19 11:06:39 +0530

    WIP
    Signed-off-by: phanirithvij <phanirithvij2000@gmail.com>

--- ui/tests/e2e/specs/constants.ts
+++ ui/tests/e2e/specs/constants.ts
@@ -1,5 +1,7 @@
 export const BASE_URL = process.env.BASE_URL || "http://127.0.0.1:3000";
-const isProd = BASE_URL.includes("netlify.app") || BASE_URL.includes("github.io") || process.env.UI_CMD == "forge-ui";
+const isProd = BASE_URL.includes("ngi.nixos.org")
+  || BASE_URL.includes("github.io")
+  || process.env.UI_CMD == "forge-ui";
 
 export const TEST_APP_NAME = process.env.TEST_APP_NAME || (isProd ? "emerge" : "mock-test");
 export const TEST_PKG_NAME = process.env.TEST_PKG_NAME || (isProd ? "emerge" : "mock-test-pkg");
